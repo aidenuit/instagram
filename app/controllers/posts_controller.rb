1e92bc8ee4cb3eae7cc27ef7eed6dd5bd89d34e0
viet <aidenle.uit@gmail.com>
2018-12-21 10:41:49 +0700

    Show Post details

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,10 @@ class PostsController < ApplicationController
     @posts = Post.all.order("created_at DESC").page(params[:page]).per(5)
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def new
     @post = Post.new
   end
