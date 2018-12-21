433d0a9bc23d3398d22f05f5de9126efe3a87c36
viet <aidenle.uit@gmail.com>
2018-12-21 09:53:05 +0700

    Include more fields to sign_up_params and account_update_params methods inside the RegistrationsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { resgistrations: 'resgistrations' }
   root 'posts#index'
 
   resources :users, only: [:show, :edit, :update]
